b0079e9368c9831cf31461063b5a4214a41d2fc7
enzo1904 <101226267+enzo1904@users.noreply.github.com>
2022-05-29 20:40:21 -0400

    Add files via upload

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import TipoServicio, Servicio 
+
 # Register your models here.
-admin.site.register(TipoServicio)
-admin.site.register(Servicio)
--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,8 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from xml.dom.minidom import Document
-from .views import home,form_Servicio,inicio, registro, barbero, modificar_servicio,delete
+from .views import index, inicio, registro, barbero
 urlpatterns = [
-    path('', home, name='admin-index' ),
-    path('index',home,name='home'),
+    path('', index, name='admin-index' ),
     path('iniciosesion/', inicio, name='iniciosesion'),
     path('registro/', registro, name='registro'),
-    path('barbero/', barbero, name='barbero'),       
-    path('form_Servicio/',form_Servicio,name='form_servicio'),
-    path('modificar-servicio/<id>',modificar_servicio,name='modificar_servicio'),
-    path('delete/<id>', delete,name='delete'),
-
+    path('barbero/', barbero, name='barbero'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
